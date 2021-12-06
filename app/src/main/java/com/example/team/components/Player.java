88260491657e4d1f53aee90771e6e3e0ae0e0d21
Fabian Hepke <fabian.hepke@stp-online.de>
2021-12-06 22:56:36 +0100

    renamed

---
+++ app/src/main/java/com/example/team/components/Player.java
@@ -0,0 +1,11 @@
+package com.example.team.components;
+
+import java.util.Date;
+
+public class Player extends User{
+
+    public Player(String benutzername, String eMail, String passwort) {
+        super(benutzername, eMail, passwort);
+    }
+
+}
