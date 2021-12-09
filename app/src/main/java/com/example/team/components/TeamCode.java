1efc5bed14085d37a22fafda87b7b503bed4c910
Fabian Hepke <fabian.hepke@stp-online.de>
2021-12-09 22:40:07 +0100

    make it as value object

--- app/src/main/java/com/example/team/components/TeamCode.java
+++ app/src/main/java/com/example/team/components/TeamCode.java
@@ -1,4 +1,17 @@
 package com.example.team.components;
 
 public class TeamCode {
+    final int code;
+
+    public TeamCode(int code) {
+        if ((Math.log10(code) + 1) != 6) {
+            throw new IllegalArgumentException("Code must have 6 digits!");
+        }
+        this.code = code;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
 }
