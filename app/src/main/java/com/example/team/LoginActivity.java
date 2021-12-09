d132535b2920d9c912a014575cf5aa201c9dd38c
Fabian Hepke <fabian.hepke@stp-online.de>
2021-12-09 22:44:16 +0100

    tried to add login feature and input check but that isn't ready yet

--- app/src/main/java/com/example/team/LoginActivity.java
+++ app/src/main/java/com/example/team/LoginActivity.java
@@ -4,22 +4,37 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.example.team.components.User;
+import com.example.team.database.PhpConnection;
+import com.example.team.help.EMail;
+import com.example.team.help.InputChecker;
+import com.example.team.help.ToastMessageHelper;
+
+import java.util.concurrent.TimeUnit;
+
+import static android.content.ContentValues.TAG;
 
 public class LoginActivity extends AppCompatActivity {
 
-
+    private EditText username, password;
     private Button login, register;
+    private CheckBox stayLoggedInBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        login = findViewById(R.id.login_loginbtn);
-        register = findViewById(R.id.login_registerbtn);
+        assignElements();
     }
-
 
 
     @Override
@@ -37,9 +52,57 @@ public class LoginActivity extends AppCompatActivity {
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, JoinOrCreateTeamActivity.class);
-                startActivity(intent);
+                User user;
+                if (InputChecker.checkLoginData(LoginActivity.this, username.getText().toString(), password.getText().toString())) {
+
+                }
+                user = getLoginUser(username.getText().toString(), password.getText().toString(), stayLoggedInBox.isChecked());
+
             }
         });
+    }
+
+    private void handleLogin(String usernaameOrEmail) {
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+
+            }
+        }, 2000);
+    }
+
+    private void goToHomeOrJoinTeam(User user) {
+        if (user.getTeamID() == null) {
+            goToJoinOrCreateTeam();
+        }
+        goToHome();
+    }
+
+    private void goToHome() {
+        Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+        startActivity(intent);
+    }
+
+    private void goToJoinOrCreateTeam() {
+        Intent intent = new Intent(LoginActivity.this, JoinOrCreateTeamActivity.class);
+        startActivity(intent);
+    }
+
+    private User getLoginUser(String usernameOrMail, String password, boolean stayLoggedIn) {
+        PhpConnection connection = new PhpConnection(this);
+        if (EMail.isValid(usernameOrMail)) {
+            return connection.login(new EMail(usernameOrMail), password, stayLoggedIn);
+        }
+        else {
+            return connection.login(usernameOrMail, password, stayLoggedIn);
+        }
+    }
+
+    private void assignElements() {
+        login = findViewById(R.id.login_loginbtn);
+        register = findViewById(R.id.login_registerbtn);
+        username = findViewById(R.id.login_username);
+        password = findViewById(R.id.login_password);
+        stayLoggedInBox = findViewById(R.id.login_angemeldetbleiben);
     }
 }
