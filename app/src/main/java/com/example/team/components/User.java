37e9d75094ac00a84ed55b8bc662ac6aed1205d0
Fabian Hepke <fabian.hepke@stp-online.de>
2021-12-09 22:35:31 +0100

    add constructors

--- app/src/main/java/com/example/team/components/User.java
+++ app/src/main/java/com/example/team/components/User.java
@@ -4,26 +4,75 @@ import com.example.team.help.Token;
 
 import java.util.Date;
 
-public abstract class User {
-    String benutzername, eMail, passwort;
+public  class User {
+    int userID;
+    String username, eMail, password;
     Token loginToken, verifyToken;
-    Date erstellungsDatum;
+    Date creationDate;
+    Rank rank;
+    TeamCode teamID;
+    boolean verfied;
 
-    public User(String benutzername, String eMail, String passwort, Date erstellungsDatum) {
-        this.benutzername = benutzername;
+    public User(int userID, String username, String eMail) {
+        this.userID = userID;
+        this.username = username;
         this.eMail = eMail;
-        this.passwort = passwort;
-        this.erstellungsDatum = erstellungsDatum;
-        loginToken = new Token(25);
-        verifyToken = new Token(25);
     }
 
-    public String getBenutzername() {
-        return benutzername;
+    public User() {
     }
 
-    public void setBenutzername(String benutzername) {
-        this.benutzername = benutzername;
+    public User(int userID, String username, String eMail, Token loginToken) {
+        this.userID = userID;
+        this.username = username;
+        this.eMail = eMail;
+        this.loginToken = loginToken;
+    }
+
+    public User(String username, String eMail, String password) {
+        this.username = username;
+        this.eMail = eMail;
+        this.password = password;
+    }
+
+    public TeamCode getTeamID() {
+        return teamID;
+    }
+
+    public void setTeamID(TeamCode teamID) {
+        this.teamID = teamID;
+    }
+
+    public boolean isVerfied() {
+        return verfied;
+    }
+
+    public void setVerfied(boolean verfied) {
+        this.verfied = verfied;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public Rank getRank() {
+        return rank;
+    }
+
+    public void setRank(Rank rank) {
+        this.rank = rank;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public String geteMail() {
@@ -34,23 +83,35 @@ public abstract class User {
         this.eMail = eMail;
     }
 
-    public String getPasswort() {
-        return passwort;
+    public String getPassword() {
+        return password;
     }
 
-    public void setPasswort(String passwort) {
-        this.passwort = passwort;
+    public void setPassword(String password) {
+        this.password = password;
     }
 
-    public Date getErstellungsDatum() {
-        return erstellungsDatum;
+    public Token getLoginToken() {
+        return loginToken;
     }
 
-    public void setErstellungsDatum(Date erstellungsDatum) {
-        this.erstellungsDatum = erstellungsDatum;
+    public void setLoginToken(Token loginToken) {
+        this.loginToken = loginToken;
     }
 
+    public Token getVerifyToken() {
+        return verifyToken;
+    }
 
+    public void setVerifyToken(Token verifyToken) {
+        this.verifyToken = verifyToken;
+    }
 
+    public Date getCreationDate() {
+        return creationDate;
+    }
 
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
 }
