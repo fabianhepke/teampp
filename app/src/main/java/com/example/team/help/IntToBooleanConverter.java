105eb1abf3e356ab6d174929ae9b9a7bb2bcdb66
Fabian Hepke <fabian.hepke@stp-online.de>
2021-12-09 22:45:51 +0100

    COnverter that converts a 1 to true and 0 to false

---
+++ app/src/main/java/com/example/team/help/IntToBooleanConverter.java
@@ -0,0 +1,10 @@
+package com.example.team.help;
+
+public class IntToBooleanConverter {
+    public static boolean convertIntToBoolean(int i) {
+        if (i>0) {
+            return true;
+        }
+        return false;
+    }
+}
