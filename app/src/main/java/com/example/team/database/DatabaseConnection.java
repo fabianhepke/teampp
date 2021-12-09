198e308044ddc057c7893ad10b3a4b6287148a0a
Fabian Hepke <fabian.hepke@stp-online.de>
2021-12-09 22:49:19 +0100

    add a database interface to make it possible to implement different dabase connections

---
+++ app/src/main/java/com/example/team/database/DatabaseConnection.java
@@ -0,0 +1,25 @@
+package com.example.team.database;
+
+import com.example.team.components.User;
+import com.example.team.help.EMail;
+
+public interface DatabaseConnection {
+
+    public void registerUser(User user);
+
+    public User login(String username, String password, boolean stayLoggedin);
+
+    public User login(EMail email, String password, boolean stayLoggedin);
+
+    public User getUserByUsername(String username);
+
+    public User getUserByEmail(String email);
+
+    boolean doesUserEmailExist(String usernameOrEmail);
+
+    boolean doesUserNameExist(String usernameOrEmail);
+
+    boolean doesPasswordMatchUsername(String usernameOrEmail, String password);
+
+    boolean doesPasswordMatchEmail(String usernameOrEmail, String password);
+}
