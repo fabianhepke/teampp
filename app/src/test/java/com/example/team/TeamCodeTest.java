17f188302c1340945e6751c4c8dfdca21a0ae550
Fabian Hepke <fabian.hepke@stp-online.de>
2022-01-13 16:09:33 +0100

    Unit Test for Value Object TeamCode

---
+++ app/src/test/java/com/example/team/TeamCodeTest.java
@@ -0,0 +1,12 @@
+package com.example.team;
+
+import com.example.team.components.TeamCode;
+
+import org.junit.Test;
+
+public class TeamCodeTest {
+    @Test
+    public void TeamCodeCostructor() {
+        TeamCode tc = new TeamCode(123456);
+    }
+}
