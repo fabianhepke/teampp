e2812cde1fd2dc0bbe21a6a0a3e9a77213db9308
Fabian Hepke <fabian.hepke@stp-online.de>
2022-01-14 16:56:48 +0100

    add new constructor

--- app/src/main/java/com/example/team/components/TeamCode.java
+++ app/src/main/java/com/example/team/components/TeamCode.java
@@ -1,5 +1,7 @@
 package com.example.team.components;
 
+import com.example.team.database.PhpConnection;
+
 public class TeamCode {
     final int code;
 
@@ -8,6 +10,15 @@ public class TeamCode {
             throw new IllegalArgumentException("Code must have 6 digits!");
         }
         this.code = code;
+    }
+
+    public TeamCode() {
+        PhpConnection con = new PhpConnection();
+        int tmp = con.getMaxTeamID() + 1;
+        if (String.valueOf(tmp).length() != 6) {
+            throw new IllegalArgumentException("Code must have 6 digits!");
+        }
+        this.code = tmp;
     }
 
     public int getCode() {
