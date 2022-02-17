90cef159c83cce03c3e0a6eb50b5542f2b9dc184
Fabian Hepke <fabian.hepke@stp-online.de>
2022-02-17 21:53:30 +0100

    make Navigation Bar work

--- app/src/main/java/com/example/team/HomeActivity.java
+++ app/src/main/java/com/example/team/HomeActivity.java
@@ -3,12 +3,41 @@ package com.example.team;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
+
+import com.example.team.components.User;
+import com.example.team.help.NavigationHandler;
 
 public class HomeActivity extends AppCompatActivity {
+
+    LinearLayout teams, home, profile;
+    User user = new User();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        assignElements();
+        getUserInfos();
+
+        NavigationHandler nav = new NavigationHandler(this, user.getUserID());
+        nav.setNavigaionBarColor(teams, home, profile, 2);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    private void getUserInfos() {
+        Bundle extra = getIntent().getExtras();
+        user.setUserID(extra.getInt("user_id"));
+    }
+
+    private void assignElements() {
+        teams = findViewById(R.id.nav_teams);
+        home = findViewById(R.id.nav_home);
+        profile = findViewById(R.id.nav_profile);
     }
 }
