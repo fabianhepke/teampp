02077c423b02070653296de3adeb178f3225b745
Fabian Hepke <fabian.hepke@stp-online.de>
2022-02-17 20:42:46 +0100

    some refactoring

--- app/src/main/java/com/example/team/JoinOrCreateTeamActivity.java
+++ app/src/main/java/com/example/team/JoinOrCreateTeamActivity.java
@@ -7,14 +7,22 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.team.components.User;
+
 public class JoinOrCreateTeamActivity extends AppCompatActivity {
 
     Button join, create;
+    User user = new User();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_join_or_create_team);
+        assignElements();
+        getUserInfos();
+    }
+
+    private void assignElements() {
         join = findViewById(R.id.joinorcreate_join);
         create = findViewById(R.id.joinorcreate_create);
     }
@@ -22,10 +30,15 @@ public class JoinOrCreateTeamActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
+        assignButtonEvents();
+    }
+
+    private void assignButtonEvents() {
         join.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(JoinOrCreateTeamActivity.this, JoinTeamActivity.class);
+                intent.putExtra("user_id", user.getUserID());
                 startActivity(intent);
             }
         });
@@ -34,8 +47,14 @@ public class JoinOrCreateTeamActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(JoinOrCreateTeamActivity.this, CreateTeamActivity.class);
+                intent.putExtra("user_id", user.getUserID());
                 startActivity(intent);
             }
         });
+    }
+
+    private void getUserInfos() {
+        Bundle extra = getIntent().getExtras();
+        user.setUserID(extra.getInt("user_id"));
     }
 }
