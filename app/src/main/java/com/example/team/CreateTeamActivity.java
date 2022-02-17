f41da55d35f09a57a92ed9ac522cb60f3d3aafef
Fabian Hepke <fabian.hepke@stp-online.de>
2022-02-17 20:43:32 +0100

    some refactoring

--- app/src/main/java/com/example/team/CreateTeamActivity.java
+++ app/src/main/java/com/example/team/CreateTeamActivity.java
@@ -10,7 +10,9 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.example.team.components.Rank;
 import com.example.team.components.Team;
+import com.example.team.components.User;
 import com.example.team.database.PhpConnection;
 
 public class CreateTeamActivity extends AppCompatActivity {
@@ -19,14 +21,32 @@ public class CreateTeamActivity extends AppCompatActivity {
     TextView teamIDView;
     Button submitBtn;
     Team team;
+    User user = new User();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_team);
 
+        getUserInfos();
         assignElements();
         createNewTeam();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        showTeamId();
+        assignButtonEvents();
+    }
+
+    private void showTeamId() {
+        teamIDView.setText(String.valueOf(team.getTeamID().getCode()));
+    }
+
+    private void getUserInfos() {
+        Bundle extra = getIntent().getExtras();
+        user.setUserID(extra.getInt("user_id"));
     }
 
     private void assignElements() {
@@ -41,18 +61,21 @@ public class CreateTeamActivity extends AppCompatActivity {
         team = new Team();
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        teamIDView.setText(String.valueOf(team.getTeamID().getCode()));
+    private void assignButtonEvents() {
         submitBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 addInfosToTeam();
                 saveTeamInDatabase();
+                saveUserTeamConnection();
                 goToHome();
             }
         });
+    }
+
+    private void saveUserTeamConnection() {
+        PhpConnection conn = new PhpConnection();
+        conn.addConnection(user.getUserID(), team.getTeamID().getCode(), Rank.PLAYERADMIN);
     }
 
     private void addInfosToTeam() {
@@ -68,6 +91,7 @@ public class CreateTeamActivity extends AppCompatActivity {
 
     private void goToHome() {
         Intent intent = new Intent(CreateTeamActivity.this, HomeActivity.class);
+        intent.putExtra("user_id", user.getUserID());
         startActivity(intent);
     }
 }
