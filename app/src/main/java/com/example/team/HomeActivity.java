9bc018efa6abcf290e7471ef47fd8700a4fba086
Fabian Hepke <fabian.hepke@stp-online.de>
2022-02-20 20:47:42 +0100

    change extra to SharedPreferences

--- app/src/main/java/com/example/team/HomeActivity.java
+++ app/src/main/java/com/example/team/HomeActivity.java
@@ -2,10 +2,14 @@ package com.example.team;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 
+import com.example.team.components.Team;
+import com.example.team.components.TeamCode;
 import com.example.team.components.User;
 import com.example.team.help.NavigationHandler;
 
@@ -13,6 +17,7 @@ public class HomeActivity extends AppCompatActivity {
 
     ImageButton teams, home, profile;
     User user = new User();
+    Team team;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,8 +38,13 @@ public class HomeActivity extends AppCompatActivity {
     }
 
     private void getUserInfos() {
-        Bundle extra = getIntent().getExtras();
-        user.setUserID(extra.getInt("user_id"));
+        SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
+        user.setUserID(sharedPref.getInt("user_id", 0));
+    }
+
+    private void getTeamInfos() {
+        SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
+        team = new Team(new TeamCode(sharedPref.getInt("team_id", 100000)));
     }
 
     private void assignElements() {
