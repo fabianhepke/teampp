e9191a28846c09b58c4d6dc27899e69a4b6407da
Fabian Hepke <fabian.hepke@stp-online.de>
2022-02-20 11:17:35 +0100

    new helper class, that prepares the Strings for the URL Syntax

---
+++ app/src/main/java/com/example/team/help/URLHelper.java
@@ -0,0 +1,24 @@
+package com.example.team.help;
+
+public class URLHelper {
+    public static String convertStringForUrl(String string) {
+        string = string.replace(" ", "%20");
+        string = string.replace("ä", "ae");
+        string = string.replace("ö", "oe");
+        string = string.replace("ü", "ue");
+        string = string.replace("Ä", "AE");
+        string = string.replace("Ö", "OE");
+        return string.replace("ü", "UE");
+    }
+
+    public static String convertUrlString(String string) {
+        string = string.replace("ae", "ä");
+        string = string.replace("oe", "ö");
+        string = string.replace("ue", "ü");
+        string = string.replace("AE", "Ä");
+        string = string.replace("OE", "Ö");
+        return string.replace("UE", "Ü");
+    }
+
+
+}
